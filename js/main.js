80c265beee5c99fd6386f7a615ea6a232f35415a
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2024-08-16 12:50:07 -0300

    Simplify javascript

--- js/main.js
+++ js/main.js
@@ -1,74 +1,38 @@
 "use strict";
 
 window.onload = async function() {
-    let comps = await fetchComponents();
-    let tools = await fetchTools();
-    let softws = await fetchSoftwares();
+    let comps = await fetchJson("json/components.json");
+    let tools = await fetchJson("json/tools.json");
+    let softws = await fetchJson("json/softwares.json");
 
     let compsList = document.getElementById("components-list");
     let toolsList = document.getElementById("tools-list");
     let softwList = document.getElementById("softwares-list");
 
-    for (let i in comps) {
+    createSelection(compsList, comps);
+    createSelection(toolsList, tools);
+    createSelection(softwList, softws);
+}
+
+function createSelection(destination, list) {
+    for (let i in list) {
         let hold = document.createElement('div');
         let checkbox = document.createElement('input');
-        checkbox.id = "chbid-" + comps[i]["id"];
+        checkbox.id = "chbid-" + list[i]["id"];
         checkbox.type = "checkbox";
-        checkbox.value = comps[i]["id"];
+        checkbox.value = list[i]["id"];
         var label = document.createElement('label');
-        var tn = document.createTextNode(comps[i]["name"]);
-        label.htmlFor="chbid-" + comps[i]["id"];
+        var tn = document.createTextNode(list[i]["name"]);
+        label.htmlFor="chbid-" + list[i]["id"];
         label.appendChild(tn);
         hold.appendChild(checkbox);
         hold.appendChild(label);
-        compsList.appendChild(hold);
-    }
-
-    for (let i in tools) {
-        let hold = document.createElement('div');
-        let checkbox = document.createElement('input');
-        checkbox.id = "chbid-" + tools[i]["id"];
-        checkbox.type = "checkbox";
-        checkbox.value = tools[i]["id"];
-        var label = document.createElement('label');
-        var tn = document.createTextNode(tools[i]["name"]);
-        label.htmlFor="chbid-" + tools[i]["id"];
-        label.appendChild(tn);
-        hold.appendChild(checkbox);
-        hold.appendChild(label);
-        toolsList.appendChild(hold);
-    }
-
-    for (let i in softws) {
-        let hold = document.createElement('div');
-        let checkbox = document.createElement('input');
-        checkbox.id = "chbid-" + softws[i]["id"];
-        checkbox.type = "checkbox";
-        checkbox.value = softws[i]["id"];
-        var label = document.createElement('label');
-        var tn = document.createTextNode(softws[i]["name"]);
-        label.htmlFor="chbid-" + softws[i]["id"];
-        label.appendChild(tn);
-        hold.appendChild(checkbox);
-        hold.appendChild(label);
-        softwList.appendChild(hold);
+        destination.appendChild(hold);
     }
 }
 
-async function fetchComponents() {
-    let response = await fetch("json/components.json");
-    let json = await response.json();
-    return json;
-}
-
-async function fetchTools() {
-    let response = await fetch("json/tools.json");
-    let json = await response.json();
-    return json;
-}
-
-async function fetchSoftwares() {
-    let response = await fetch("json/softwares.json");
+async function fetchJson(filePath) {
+    let response = await fetch(filePath);
     let json = await response.json();
     return json;
 }
