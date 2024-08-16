43f6db78c3c5b78589a3e74098f285ca4a1a3511
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2024-08-16 13:05:47 -0300

    Migrate to 2 spaces identation

--- js/main.js
+++ js/main.js
@@ -1,71 +1,71 @@
 "use strict";
 
 window.onload = async function() {
-    let comps = await fetchJson("json/components.json");
-    let tools = await fetchJson("json/tools.json");
-    let softws = await fetchJson("json/softwares.json");
+  let comps = await fetchJson("json/components.json");
+  let tools = await fetchJson("json/tools.json");
+  let softws = await fetchJson("json/softwares.json");
 
-    let compsList = document.getElementById("components-list");
-    let toolsList = document.getElementById("tools-list");
-    let softwList = document.getElementById("softwares-list");
+  let compsList = document.getElementById("components-list");
+  let toolsList = document.getElementById("tools-list");
+  let softwList = document.getElementById("softwares-list");
 
-    createSelection(compsList, comps);
-    createSelection(toolsList, tools);
-    createSelection(softwList, softws);
+  createSelection(compsList, comps);
+  createSelection(toolsList, tools);
+  createSelection(softwList, softws);
 }
 
 function createSelection(destination, list) {
-    for (let i in list) {
-        let hold = document.createElement('div');
-        hold.className = "selection-item"
-        let checkbox = document.createElement('input');
-        checkbox.id = "chbid-" + list[i]["id"];
-        checkbox.type = "checkbox";
-        checkbox.value = list[i]["id"];
-        let label = document.createElement('label');
-        let tn = document.createTextNode(list[i]["name"]);
-        label.htmlFor="chbid-" + list[i]["id"];
-        label.appendChild(tn);
-        hold.appendChild(checkbox);
-        hold.appendChild(label);
-        destination.appendChild(hold);
-    }
+  for (let i in list) {
+    let hold = document.createElement('div');
+    hold.className = "selection-item"
+    let checkbox = document.createElement('input');
+    checkbox.id = "chbid-" + list[i]["id"];
+    checkbox.type = "checkbox";
+    checkbox.value = list[i]["id"];
+    let label = document.createElement('label');
+    let tn = document.createTextNode(list[i]["name"]);
+    label.htmlFor="chbid-" + list[i]["id"];
+    label.appendChild(tn);
+    hold.appendChild(checkbox);
+    hold.appendChild(label);
+    destination.appendChild(hold);
+  }
 }
 
 async function fetchJson(filePath) {
-    let response = await fetch(filePath);
-    let json = await response.json();
-    return json;
+  let response = await fetch(filePath);
+  let json = await response.json();
+  return json;
 }
 
 function generateWingetCommand() {
-    let wgSection = document.getElementById("winget-section");
-    wgSection.innerHTML = ""
+  let wgSection = document.getElementById("winget-section");
+  wgSection.innerHTML = ""
 
-    let selectedComponents = [...document.getElementById("components-list").querySelectorAll(":checked")].map(e => e.value);
-    let selectedTools = [...document.getElementById("tools-list").querySelectorAll(":checked")].map(e => e.value);
-    let selectedSoftwares = [...document.getElementById("softwares-list").querySelectorAll(":checked")].map(e => e.value);
+  let selectedComponents = [...document.getElementById("components-list").querySelectorAll(":checked")].map(e => e.value);
+  let selectedTools = [...document.getElementById("tools-list").querySelectorAll(":checked")].map(e => e.value);
+  let selectedSoftwares = [...document.getElementById("softwares-list").querySelectorAll(":checked")].map(e => e.value);
 
-    let fullSelectionList = [...selectedComponents, ...selectedTools, ...selectedSoftwares];
-    if (fullSelectionList.length == 0) {
-        alert("No items selected");
-        return;
-    }
-    let output = "winget install " + fullSelectionList.join(" ");
+  let fullSelectionList = [...selectedComponents, ...selectedTools, ...selectedSoftwares];
+  if (fullSelectionList.length == 0) {
+    alert("No items selected");
+    return;
+  }
+  let output = "winget install " + fullSelectionList.join(" ");
 
-    let hold = document.createElement('article');
-    let div = document.createElement('div');
-    let codeBlock = document.createElement('code');
-    let commandText = document.createTextNode(output);
-    codeBlock.appendChild(commandText);
-    div.appendChild(codeBlock);
-    let img = document.createElement('img');
-    img.src = "img/copy.svg";
-    img.onclick = function() {
-        navigator.clipboard.writeText(output);
-        alert("Copied to clipboard: " + output);
-    }
-    div.appendChild(img);
-    hold.appendChild(div);
-    wgSection.appendChild(hold);
+  let hold = document.createElement('article');
+  let div = document.createElement('div');
+  let codeBlock = document.createElement('code');
+  let commandText = document.createTextNode(output);
+  codeBlock.appendChild(commandText);
+  div.appendChild(codeBlock);
+  let img = document.createElement('img');
+  img.src = "img/copy.svg";
+  img.onclick = function() {
+    navigator.clipboard.writeText(output);
+    alert("Copied to clipboard: " + output);
+  }
+  div.appendChild(img);
+  hold.appendChild(div);
+  wgSection.appendChild(hold);
 }
